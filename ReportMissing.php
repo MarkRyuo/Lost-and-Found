3f2f8ffcb6f1fedb1429a5504ddfb4bdbbffbbc3
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-17 21:07:40 +0800

    update here

---
+++ ReportMissing.php
@@ -0,0 +1,62 @@
+<?php
+// Replace these variables with your actual database connection details
+$servername = "your_server_name";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect user input
+    $itemName = $_POST["item_name"];
+    $description = $_POST["description"];
+    $category = $_POST["category"];
+    $foundLocation = $_POST["found_location"];
+    $foundDate = $_POST["found_date"];
+    $ownerID = $_POST["owner_id"]; // Assuming you have a way to identify the user, like through login
+
+    // Insert the new item into the Items table
+    $sql = "INSERT INTO Items (ItemName, Description, Category, FoundLocation, FoundDate, OwnerID)
+            VALUES ('$itemName', '$description', '$category', '$foundLocation', '$foundDate', $ownerID)";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Item reported successfully!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Close the database connection
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Report Missing Item</title>
+</head>
+<body>
+
+<h2>Report Missing Item</h2>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    Item Name: <input type="text" name="item_name" required><br>
+    Description: <textarea name="description" required></textarea><br>
+    Category: <input type="text" name="category" required><br>
+    Found Location: <input type="text" name="found_location" required><br>
+    Found Date: <input type="date" name="found_date" required><br>
+    <!-- You might want to use a session variable or other means to get the user's ID -->
+    Owner ID: <input type="text" name="owner_id" required><br>
+    <input type="submit" value="Report Item">
+</form>
+
+</body>
+</html>
