07689ff9d3cf808fdb47727759ce84406c114019
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-17 21:56:28 +0800

    update here

--- Viewlostitem.php
+++ Viewlostitem.php
@@ -28,40 +28,41 @@ $result = $conn->query($sql);
 
 <h2>Lost Items</h2>
 
-<table border="1">
-    <tr>
-        <th>Item ID</th>
-        <th>Item Name</th>
-        <th>Description</th>
-        <th>Category</th>
-        <th>Found Location</th>
-        <th>Found Date</th>
-    </tr>
-
-    <?php
-    // Display lost items in a table
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>" . $row["ItemID"] . "</td>";
-            echo "<td>" . $row["ItemName"] . "</td>";
-            echo "<td>" . $row["Description"] . "</td>";
-            echo "<td>" . $row["Category"] . "</td>";
-            echo "<td>" . $row["FoundLocation"] . "</td>";
-            echo "<td>" . $row["FoundDate"] . "</td>";
-            echo "</tr>";
-        }
-    } else {
-        echo "<tr><td colspan='6'>No lost items found.</td></tr>";
-    }
-    ?>
-
-</table>
-
-</body>
-</html>
-
 <?php
+// Debugging output: Display the actual SQL query being executed
+echo "SQL Query: " . $sql . "<br>";
+
+// Display lost items in a table
+if ($result->num_rows > 0) {
+    echo "<table border='1'>";
+    echo "<tr>";
+    echo "<th>Item ID</th>";
+    echo "<th>Item Name</th>";
+    echo "<th>Description</th>";
+    echo "<th>Category</th>";
+    echo "<th>Found Location</th>";
+    echo "<th>Found Date</th>";
+    echo "</tr>";
+
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["ItemID"] . "</td>";
+        echo "<td>" . $row["ItemName"] . "</td>";
+        echo "<td>" . $row["Description"] . "</td>";
+        echo "<td>" . $row["Category"] . "</td>";
+        echo "<td>" . $row["FoundLocation"] . "</td>";
+        echo "<td>" . $row["FoundDate"] . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+} else {
+    echo "No lost items found.";
+}
+
 // Close the database connection
 $conn->close();
 ?>
+
+</body>
+</html>
