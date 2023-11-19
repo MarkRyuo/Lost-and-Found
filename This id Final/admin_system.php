18ab0e15a8d59bc18e16aaddd685d63bfd960be6
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-19 10:15:04 +0800

    update here

--- This id Final/admin_system.php
+++ This id Final/admin_system.php
@@ -1,3 +1,23 @@
+<?php
+session_start();
+require_once('db.php'); // Include the database connection file
+
+if (!isset($_SESSION['username'])) {
+    header('Location: index.html');
+    exit;
+}
+
+if ($_SESSION['role'] !== 'admin') {
+    // Redirect if the user is not an admin
+    header('Location: security_system.php');
+    exit;
+}
+
+// Retrieve user information from the session
+$username = $_SESSION['username'];
+$fullName = $_SESSION['full_name'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +27,14 @@
 </head>
 <body>
     <h2>Welcome Admin!</h2>
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" value="<?php echo htmlspecialchars($username); ?>" readonly>
+
+    <label for="fullName">Full Name:</label>
+    <input type="text" id="fullName" name="fullName" value="<?php echo htmlspecialchars($fullName); ?>" readonly>
+
     <!-- Your admin system content goes here -->
     <a href="/This id Final/Register.html">Register</a>
 </body>
 </html>
+
