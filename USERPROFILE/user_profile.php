d8a2af5ea509920d3b5fd7dee9b2f7ea309e8af0
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-19 08:54:05 +0800

    update here

--- USERPROFILE/user_profile.php
+++ USERPROFILE/user_profile.php
@@ -1,41 +1,44 @@
 <?php
 session_start();
 
-// Check if the form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Include your database connection file
-    include_once("db_connection.php");
-
-    // Get user input
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $password = mysqli_real_escape_string($conn, $_POST['password']);
-
-    // Query the database
-    $query = "SELECT * FROM users WHERE username = '$username'";
-    $result = mysqli_query($conn, $query);
-
-    // Check if the user exists
-    if ($result && mysqli_num_rows($result) > 0) {
-        $row = mysqli_fetch_assoc($result);
-
-        // Verify the password
-        if (password_verify($password, $row['password'])) {
-            // Password is correct, set session
-            $_SESSION['user_id'] = $row['user_id'];
-
-            // Redirect to the user profile page
-            header("Location: user_profile.php");
-            exit();
-        } else {
-            // Password is incorrect
-            echo "Invalid password";
-        }
-    } else {
-        // User does not exist
-        echo "Invalid username";
-    }
-
-    // Close the database connection
-    mysqli_close($conn);
+// Check if the user is logged in
+if (!isset($_SESSION['user_id'])) {
+    header("Location: index.html");
+    exit();
 }
+
+// Include your database connection file
+include_once("db_connection.php");
+
+// Get the user information from the database
+$user_id = $_SESSION['user_id'];
+$query = "SELECT * FROM users WHERE user_id = '$user_id'";
+$result = mysqli_query($conn, $query);
+
+if ($result && mysqli_num_rows($result) > 0) {
+    $user = mysqli_fetch_assoc($result);
+} else {
+    echo "Error fetching user information";
+    exit();
+}
+
+// Close the database connection
+mysqli_close($conn);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profile</title>
+</head>
+<body>
+    <h2>User Profile</h2>
+    <p>Welcome, <?php echo $user['username']; ?>!</p>
+    <!-- Display other user information as needed -->
+    <p>Email: <?php echo $user['email']; ?></p>
+    <p>Full Name: <?php echo $user['full_name']; ?></p>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
